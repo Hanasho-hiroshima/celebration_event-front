5c468d33b8c22326c067094ecd063200bee4fa7c
okmkm321 <okmkm321@gmail.com>
2022-12-22 23:59:33 +0900

    autocomplete作成

---
+++ packages/components-next/components/input/InputAutocomplete/InputAutocomplete.stories.tsx
@@ -0,0 +1,119 @@
+import React, { useState } from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { InputAutocomplete } from './InputAutocomplete'
+import { action } from '@storybook/addon-actions'
+
+type SampleItem = {
+  key: string
+  label: string
+  disabled?: boolean
+}
+
+type InputAutocompleteType = typeof InputAutocomplete<SampleItem>
+
+const ITEMS: SampleItem[] = [
+  {
+    key: 'train',
+    label: '電車',
+  },
+  {
+    key: 'bus',
+    label: 'バス',
+  },
+  {
+    key: 'car',
+    label: '車',
+  },
+  {
+    key: 'bicycle',
+    label: '自転車',
+  },
+  {
+    key: 'walk',
+    label: '徒歩',
+  },
+]
+
+const DISABLED_ITEMS: SampleItem[] = [
+  {
+    key: 'train',
+    label: '電車',
+  },
+  {
+    key: 'bus',
+    label: 'バス',
+    disabled: true,
+  },
+  {
+    key: 'car',
+    label: '車',
+  },
+  {
+    key: 'bicycle',
+    label: '自転車',
+    disabled: true,
+  },
+  {
+    key: 'walk',
+    label: '徒歩',
+  },
+]
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Input/Autocomplete',
+  component: InputAutocomplete,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  args: {
+    value: [{ ...ITEMS[0] }],
+    items: ITEMS,
+    itemLabelKey: 'label',
+    itemValueKey: 'key',
+    multiple: true,
+    onSetItem: action('onSetItem'),
+  },
+} as ComponentMeta<InputAutocompleteType>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<InputAutocompleteType> = (args) => {
+  const [values, setValues] = useState(args.value)
+  return <InputAutocomplete {...args} value={values} onSetItem={setValues} />
+}
+
+export const Base = Template.bind({})
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  disabled: true,
+}
+
+export const Readonly = Template.bind({})
+Readonly.args = {
+  readonly: true,
+}
+
+export const Single = Template.bind({})
+Single.args = {
+  value: { key: 'car', label: '車' },
+  multiple: false,
+}
+
+export const ItemHasDisabled = Template.bind({})
+ItemHasDisabled.args = {
+  items: DISABLED_ITEMS,
+}
+
+export const Medium = Template.bind({})
+Medium.args = {
+  size: 'medium',
+}
+
+export const FixedChip = Template.bind({})
+FixedChip.args = {
+  fixedSelectedItem: [ITEMS[0]],
+}
+
+export const Loading = Template.bind({})
+Loading.args = {
+  loading: true,
+}
